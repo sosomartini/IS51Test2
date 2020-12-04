b787c6eeb30c36c7701461e669e3a3547b3b4a5d
Eseosa Otasowie <sosomartini@mail.fresnostate.edu>
2020-12-03 17:15:06 -0800

    finished exam-end

--- test_2.py
+++ test_2.py
@@ -7,7 +7,6 @@ Then find percentage of grades is greater than the average grades
 """
 
 """
-main(): 
    
  open text file
 grades = infile input()
@@ -18,6 +17,21 @@ print number of grades to user
 print average grade 
 print percentage of grades above average
 
-main()
 
 """
+
+infile = open("final.txt", "r")
+grades = [line.rstrip() for line in infile]
+infile.close()
+for i in range(len(grades)):
+    grades[i] = int(grades[i])
+average = sum(grades) / len(grades)
+num = 0
+
+for grade in grades: 
+    if grade > average:
+        num += 1
+
+print("Number of grades:", len(grades))
+print("Average grade:", average)
+print("percentage of grades above average: {0:.2f}%" .format(100 * num / len(grades)))
