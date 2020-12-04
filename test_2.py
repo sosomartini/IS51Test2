c544738040d6fa3d5450b0b2ce321861f50f4c53
Eseosa Otasowie <sosomartini@mail.fresnostate.edu>
2020-12-03 16:57:15 -0800

    third commit

--- test_2.py
+++ test_2.py
@@ -5,3 +5,19 @@ The program would display average grades
 Then find percentage of grades is greater than the average grades
 
 """
+
+"""
+main(): 
+   
+ open text file
+grades = infile input()
+average = sum of grades / grades 
+    if the % of grades > average
+    num = 1
+print number of grades to user 
+print average grade 
+print percentage of grades above average
+
+main()
+
+"""
