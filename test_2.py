c0ee544786043823136f715ea19de2a3bcb6816b
Eseosa Otasowie <sosomartini@mail.fresnostate.edu>
2020-12-03 16:50:07 -0800

    second commit

---
+++ test_2.py
@@ -0,0 +1,7 @@
+
+"""
+This program is to display the number of grades of final exam
+The program would display average grades 
+Then find percentage of grades is greater than the average grades
+
+"""
